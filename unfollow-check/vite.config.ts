73bfdfb0be0d4a96fddbddbfbc0667ae6d274e73
jiyun0220 <145185712+jiyun0220@users.noreply.github.com>
2025-04-01 12:14:50 +0900

    netlify4

--- unfollow-check/vite.config.ts
+++ unfollow-check/vite.config.ts
@@ -1,21 +1,20 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // .env 파일 로드
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    define: {
-      'process.env.VITE_GITHUB_TOKEN': JSON.stringify(env.VITE_GITHUB_TOKEN),
-    },
-    plugins: [react()],
-    base: './',
-    build: {
-      outDir: 'dist',
-      emptyOutDir: true,
-      sourcemap: true
-    }
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  server: {
+    port: 3000,
+    host: true
+  },
+  define: {
+    'process.env.VITE_GITHUB_TOKEN': JSON.stringify(process.env.VITE_GITHUB_TOKEN)
   }
 })
