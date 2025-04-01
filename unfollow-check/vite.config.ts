e8b95ead4991db49b91e3b50abc27a33034a2c90
jiyun0220 <145185712+jiyun0220@users.noreply.github.com>
2025-04-01 12:05:01 +0900

    netlify2

--- unfollow-check/vite.config.ts
+++ unfollow-check/vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.VITE_GITHUB_TOKEN': JSON.stringify(process.env.VITE_GITHUB_TOKEN)
+  },
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
